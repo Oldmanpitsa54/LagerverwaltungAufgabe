186159b329b9cde95f7836eef8507dc33056db95
Ivan <bublik12367@gmail.com>
2023-10-19 16:15:06 +0200

    Preparring of testing environment

---
+++ UnitTests/ArtikelgruppeCRUDTests.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class ArtikelgruppeCRUDTests
+    {
+        public async Task CreateArtikelgruppe()
+        {
+
+        }
+        public async Task GeteArtikelgruppe()
+        {
+
+        }
+        public async Task UpdateArtikelgruppe()
+        {
+
+        }
+        public async Task DeleteArtikelgruppe()
+        {
+
+        }
+    }
+}
